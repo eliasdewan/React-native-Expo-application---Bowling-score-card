b1ecb41fecf4e4c32f5833b01d06c9b25dbed820
Elias Dewan <k2056101@kingston.ac.uk>
2022-11-10 09:58:14 +0000

    folders created

--- App.js
+++ App.js
@@ -7,6 +7,8 @@ export default function App() {
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+  //item provider -> navigation container
+    //stack navigator->Stack screen
   );
 }
 
