4f06285051fb0ba25aaf3c04cf1bb16a7398e089
Elias <k2056101@kingston.ac.uk>
2022-11-22 11:18:54 +0000

    Implemented persistent storage

--- src/contexts/GameContext.js
+++ src/contexts/GameContext.js
@@ -1,5 +1,8 @@
 import { useState } from "react";
-import React, { useReducer } from "react";
+import React, { useReducer ,useEffect} from "react";
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+const STORAGE_KEY = "my_super_secret_key";
 
 const GameContext = React.createContext();
 let gmaList = [];
@@ -7,17 +10,30 @@ let gmaList = [];
 export const GameProvider = ({ children }) => {
     const [state, dispatch] = useReducer(reducer, gmaList);
 
+    useEffect(() => {
+        const loadStorage = async () => {
+            const storage = await AsyncStorage.getItem(STORAGE_KEY);
+            if (storage !== null && state.length === 0) {
+                gmaList = JSON.parse(storage);
+                gmaList.forEach(element => {
+                    dispatch({ type: "load", payload: element });
+                })
+            }
+        }
+        loadStorage();
+    }, [STORAGE_KEY])
+
     const addGame = (gameName, date, rink, playerNames, teamName, callback) => {
         console.log("Add game Received = ", gameName, date, rink, playerNames, teamName, callback);
         dispatch({ type: "create", payload: { gameName, date, rink, playerNames, teamName } });
         //dispatch({ type: actionTypes.create, payload: { gameName, date, rink, playerNames, setTeamName } });
-        //dispatch({ type: actionTypes.save });
+        dispatch({ type: "save" });
         if (callback) { callback(); }
     }
 
     const deleteGame = (id, callback) => {
         dispatch({ type: "remove", payload: { id: id } });
-        // dispatch({ type: actionTypes.save });
+        dispatch({ type: "save" });
         if (callback) callback();
     }
 
@@ -26,21 +42,25 @@ export const GameProvider = ({ children }) => {
         console.log(gameName, date, rink, playerNames, teamName, callback);
 
         dispatch({ type: "edit", payload: { id, gameName, date, rink, playerNames, teamName, end } })
+        dispatch({ type: "save" });
         if (callback) { callback(); }
     }
     const addScoreEnd = (id, teamIndex, score, callback) => {
         console.log({ type: "addScore", payload: { id, teamIndex, score } });
         dispatch({ type: "addScore", payload: { id, teamIndex, score } })
+        dispatch({ type: "save" });
         if (callback) { callback(); }
 
     }
     const editScoreEnd = (id, teamIndex, score, index, callback) => {
         dispatch({ type: "editEnd", payload: { id, teamIndex, score, index } })
+        dispatch({ type: "save" });
         if (callback) { callback(); }
     }
     const removeScoreEnd = (id, index, callback) => {
-        
+
         dispatch({ type: "removeEnd", payload: { id, index } })
+        dispatch({ type: "save" });
         if (callback) { callback(); }
 
     }
@@ -70,6 +90,27 @@ const reducer = (state, action) => {
     switch (action.type) {
         default:
             return state;
+
+        case "save":
+            try {
+                AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(state));
+            } catch (error) {
+                console.log(error);
+            } finally {
+                return state;
+            }
+        case "load":
+            return [
+                ...state, {
+                    id: action.payload.id,
+                    gameName: action.payload.gameName,
+                    date: action.payload.date,
+                    rink: action.payload.rink,
+                    playerNames: action.payload.playerNames,
+                    teamName: action.payload.teamName,
+                    end: action.payload.end
+                }
+            ]
 
         case "create":
             state.find((game) => {
